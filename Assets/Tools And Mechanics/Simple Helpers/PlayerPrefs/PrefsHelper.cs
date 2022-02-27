d2260ac274fc133e67e5b4889fbe65f98e21071c
Tityxx <nikitka_nik1@mail.ru>
2022-02-28 00:43:15 +0700

    Закончил работу над менеджером окон

---
+++ Assets/Tools And Mechanics/Simple Helpers/PlayerPrefs/PrefsHelper.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+
+namespace ToolsAndMechanics
+{
+    /// <summary>
+    /// Оболочка для PlayerPrefs
+    /// </summary>
+    public static class PrefsHelper
+    {
+        public static void SetInt(string key, int value)
+        {
+            PlayerPrefs.SetInt(key, value);
+            PlayerPrefs.Save();
+        }
+
+        public static void SetFloat(string key, float value)
+        {
+            PlayerPrefs.SetFloat(key, value);
+            PlayerPrefs.Save();
+        }
+
+        public static void SetString(string key, string value)
+        {
+            PlayerPrefs.SetString(key, value);
+            PlayerPrefs.Save();
+        }
+
+        public static void SetBool(string key, bool value)
+        {
+            PlayerPrefs.SetInt(key, value ? 1 : 0);
+            PlayerPrefs.Save();
+        }
+
+        public static int GetInt(string key, int defaultValue = 0)
+        {
+            return PlayerPrefs.GetInt(key, defaultValue);
+        }
+
+        public static float GetFloat(string key, float defaultValue = 0)
+        {
+            return PlayerPrefs.GetFloat(key, defaultValue);
+        }
+
+        public static string GetString(string key, string defaultValue = default)
+        {
+            return PlayerPrefs.GetString(key, defaultValue);
+        }
+
+        public static bool GetBool(string key, bool defaultValue = false)
+        {
+            return PlayerPrefs.GetInt(key, defaultValue ? 1 : 0) == 1 ? true : false;
+        }
+
+        public static bool HasKey(string key)
+        {
+            return PlayerPrefs.HasKey(key);
+        }
+
+        public static void DeleteAllKeys()
+        {
+            PlayerPrefs.DeleteAll();
+            PlayerPrefs.Save();
+        }
+
+        public static void DeleteKey(string key)
+        {
+            PlayerPrefs.DeleteKey(key);
+            PlayerPrefs.Save();
+        }
+    }
+}
